f409e8bb694380761345bba4db7e9fe69c0a71c4
VictorDutraa <149853348+VictorDutraa@users.noreply.github.com>
2024-03-04 12:50:38 -0300

    project done

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-/*let textoCopiado = document.getElementById("texto");
 
-navigator.clipboard.writeText(textoCopiado.value);*/
 
 /*
 As "chaves" de criptografia que utilizaremos são:
@@ -13,52 +11,66 @@ A letra "u" é convertida para "ufat"
   
 
 /*
--se  eu estiver em (1), palavra eh um texto puro
--novaPalavra eh um texto criptografado
+-se  eu estiver em (1), texto esta puro
+-novoTexto esta criptografado
 
--se eu estiver em (2), palavra eh um texto criptografado
--novaPalavra eh um texto descriptografado
+-se eu estiver em (2), texto esta criptografado
+-novoTexto esta descriptografado
 */
-let palavra = "victor";
-let texto = 
 
-//---(1) criptografando
-function Criptografa (palavra){
-    let novaPalavra = [];
-    for(let i=0; i < palavra.length; i++){
-        if(palavra[i] == 'a'){
-            novaPalavra.push("ai");
-            continue;
-        }
-        else if(palavra[i] == 'e'){
-            novaPalavra.push("enter");
-            continue;
-        }
-        else if(palavra[i] == 'i'){
-            novaPalavra.push("imes");
-            continue;
-        }
-        else if(palavra[i] == 'o'){
-            novaPalavra.push("ober");
-            continue;
-        }
-        else if(palavra[i] == 'u'){
-            novaPalavra.push("ufat");
-            continue;
-        }
-        novaPalavra.push(palavra[i]);
+function AlteraDisplayTexto (text){
+    let saida = document.getElementsByClassName("output-box__text")[0];
+    saida.innerHTML = text;
+    let  telaVaziaInicial = document.getElementsByClassName("output-empty-box")[0];
+    let telaTexto = document.getElementsByClassName("output-box")[0];
+
+    if(text.length == 0){
+        telaVaziaInicial.style.display = 'flex';
+        telaTexto.style.display = 'none';
     }
-    let novaPalavraConcatenada = "";
-    for(let i = 0; i < novaPalavra.length; i++){
-        novaPalavraConcatenada += novaPalavra[i];
-    }   
-    return novaPalavraConcatenada;
+    else{
+        telaTexto.style.display = 'flex';
+        telaVaziaInicial.style.display = 'none';
+    }
+
+    let botao = document.getElementsByClassName("output-box__copy-text-button")[0];
+
+    botao.style.border = "1px solid black";
+    
 }
 
+//---(1) criptografando -- no clique do botao criptografar
+function CriptografaTexto (){
+    let novoTexto = "";
+    let texto = document.getElementById("texto").value;
+    
+    for(let i=0; i < texto.length; i++){
+        
+        if(texto[i] == 'a'){
+            novoTexto += "ai";
+        }
+        else if(texto[i] == 'e'){
+            novoTexto += "enter";
+        }
+        else if(texto[i] == 'i'){
+            novoTexto += "imes"
+        }
+        else if(texto[i] == 'o'){
+            novoTexto += "ober";
+        }
+        else if(texto[i] == 'u'){
+            novoTexto += "ufat";
+        }
+        else {
+            novoTexto += texto[i];
+        }
+    } 
+    AlteraDisplayTexto(novoTexto);
+}
 
-
-//---(2) descriptografando
+//---(2) descriptografando -- no clique do botao descriptografar
 /*
+
 se a letra for a, eu itero 2 vezes e confirmo se eh uma msg criptografada
 
 se a letra for e eu itero 5 vezes
@@ -67,25 +79,105 @@ se a letra for i, eu itero 4 vezes, e assim vai
 
 dai se realmente for um codigo criptografado, eu pusho pra uma nova string a letra correspondente
 */
+function DescriptografaTexto (){
 
-let estaCriptografado = 0;
-let trecho = "";
-for(let i=0; i < palavra.length; i++){
+    let trecho = "";
+    let novoTexto = "";
+    let texto = document.getElementById("texto");
 
-    if(palavra[i] == 'a'){
-        trecho = "a";
-        //busco o codigo correspondente logo a frente da vogal
-        //(i + "codigo".length) significa (posicao atual + "o quanto devo andar")
-        //começo em i+1 pq eu ja sei que oq ta em i é o 'a'
-        for(let j = i+1;
-            j < i + "ai".length;
-            j++){
-            //verificando se o trecho em questao esta criptografado
-            trecho += palavra[j];
+    for(let i=0; i < texto.value.length; i++){
+
+        trecho = "";
+        if(texto.value[i] == 'a'){
+            trecho = "a";
+            /*
+            -busco o codigo correspondente logo a frente da vogal
+            -(i + "codigo".length) significa (posicao atual + "o quanto devo andar")
+            -começo em i+1 pq eu ja sei que oq ta em i é o 'a'
+            */
+
+            for(let j = i+1;
+                j < i + "ai".length;
+                j++){
+                //verificando se o trecho em questao esta criptografado
+                trecho += texto.value[j];
+            }
+            if(trecho == "ai"){
+                //pulo no for da palavra pro final do codigo
+                i += "ai".length-1;
+            }
+            novoTexto += "a"
+        }
+        else if(texto.value[i] == 'e'){
+            trecho = "e";
+            for(let j = i+1;
+                j < i + "enter".length;
+                j++){
+                trecho += texto.value[j];
+            }
+            if(trecho == "enter"){
+                i += "enter".length-1;
+            }
+            novoTexto += "e";
+        }
+        else if(texto.value[i] == 'i'){
+            trecho = "i";
+            for(let j = i+1;
+                j < i + "imes".length;
+                j++){
+                trecho += texto.value[j];
+            }
+            if(trecho == "imes"){
+                i += "imes".length-1;
+            }
+            novoTexto += "i";
+        }
+        else if(texto.value[i] == 'o'){
+            trecho = "o";
+            for(let j = i+1;
+                j < i + "ober".length;
+                j++){
+                trecho += texto.value[j];
+            }
+            if(trecho == "ober"){
+                i += "ober".length-1;
+            }
+            novoTexto += "o";
+        }
+        else if(texto.value[i] == 'u'){
+            trecho = "u";
+            for(let j = i+1;
+                j < i + "ufat".length;
+                j++){
+                trecho += texto.value[j];
+            }
+            if(trecho == "ufat"){
+                i += "ufat".length-1;
+            }
+            novoTexto += "u";
+        }
+        else {
+            novoTexto += texto.value[i];
         }
     }
+    
+    AlteraDisplayTexto(novoTexto);
 }
 
-//tenho que pegar o texto, separar por palavras, e cada palavra chamar a função correspondente
+function ControlC (){
+    let textoCopiado = document.getElementsByClassName("output-box__text")[0]; 
 
+    let botao = document.getElementsByClassName("output-box__copy-text-button")[0];
+
+    botao.style.border = "1px solid green";
+    navigator.clipboard.writeText(textoCopiado.innerText);
+}
+
+    let cripButton = document.getElementsByClassName("botoes__crip")[0];
+    let descripButton = document.getElementsByClassName("botoes__descrip")[0];
+    cripButton.addEventListener("click", CriptografaTexto);
+    descripButton.addEventListener("click", DescriptografaTexto);
+
+    let copyTextButton = document.getElementsByClassName("output-box__copy-text-button")[0];
+    copyTextButton.addEventListener("click", ControlC);
 
